fe5d5169198cf8216707c30c2420356eae5f6711
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-05-26 21:50:46 -0300

    atividade 01

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Products() {
+    return (
+        <div>
+            <ul>
+                <li>Caixa de Leite, 7,00R$</li>
+                <li>Banana 1kg, 9,00R$</li>
+                <li>Shampoo Salon LIne, 15,00R$</li>
+            </ul>
+        </div>
+    )
+}
