250e6ecfede418dcb89aee6a3655440e3581c037
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-07-21 20:36:20 -0300

    feat: google auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,15 @@
 // import { ThemeProvider } from 'next-themes'
 import './globals.css'
 
+import { GoogleOAuthProvider } from  '@react-oauth/google'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className=''>
+        <GoogleOAuthProvider clientId='process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! || ""'>
           {children}
+        </GoogleOAuthProvider>
       </body>
     </html>
   )
