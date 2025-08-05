fdab558efe52d7a107a75c50e236133c0d3f599c
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-08-04 21:26:17 -0300

    feat: card component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,31 @@
+import { Title, Cards } from "../components/Button";
+
  export default function About() {
     return (
-        <div>
-            <h1>Sobre nós</h1>
-            <p>Informações sobre a empresa...</p>
+        <div className="flex bg-neutral-50 min-h-screen flex-col justify-center items-center space-y-4">
+            <Title/>
+            <div className="flex flex-row gap-12">
+                <Cards 
+                imageUrl="https://images5.alphacoders.com/338/338156.jpg"
+                title="Tomb Raider - Survivor is born"
+                description="Tomb Raider explora a intensa história da origem de Lara Croft, e sua ascensão de uma jovem mulher a uma sobrevivente."
+                butLabel="Comprar"
+                butUrl="https://store.steampowered.com/app/203160/Tomb_Raider/"/>
+                <Cards 
+                imageUrl="https://images8.alphacoders.com/107/1074668.jpg"
+                title="Rise of the Tomb Raider"
+                description="Lara Croft viaja à Sibéria em busca da Fonte Divina, enfrentando a Trindade para descobrir a verdade sobre a imortalidade."
+                butLabel="Comprar"
+                butUrl="https://store.steampowered.com/app/391220/Rise_of_the_Tomb_Raider/"
+                />
+                <Cards 
+                imageUrl="https://images8.alphacoders.com/107/thumb-1920-1074713.jpg"
+                title="Shadow of the Tomb Raider"
+                description="Lara viava até a América Latina, onde enfrenta a Trindade e um apocalipse maia, tornando-se a verdadeira Tomb Raider."
+                butLabel="Comprar"
+                butUrl="https://store.steampowered.com/app/750920/Shadow_of_the_Tomb_Raider_Definitive_Edition/"
+                />
+            </div>
         </div>
     )
  }
