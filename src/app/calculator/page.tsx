a88fbc72f3f518811e7fd544be4d32ce980a3fa6
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-08-14 19:36:17 -0300

    calculator

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,11 @@
+import MiniDisplay from "../components/miniDisplay";
+
+ export default function DisplayPage() {
+    return(
+        <div className="flex flex-col items-center mt-35">
+            <div className="">
+                <MiniDisplay/>
+            </div>
+        </div>
+    )
+ }
