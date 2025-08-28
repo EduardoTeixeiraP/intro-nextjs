0526981dbbe9c7b4c68c18ad94002cba5cea4093
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-08-28 17:58:19 -0300

    Lista

---
+++ src/app/list/page.tsx
@@ -0,0 +1,9 @@
+import ListaDeCompras from "../components/LIstaCompras";
+
+export default function List() {
+    return(
+        <div className="flex justify-center mt-5">
+            <ListaDeCompras/>
+        </div>
+    )
+}
